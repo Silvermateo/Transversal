70ec338660943558ca948226cc571434d8f6ca3f
Rodrigo Ferron <ro797749@gmail.com>
2024-05-26 20:56:39 -0300

    v2.2
    Acceso a datos y entidades

--- src/universidadg12/accesoadatos/accesoadatos/AlumnoData.java
+++ src/universidadg12/accesoadatos/accesoadatos/AlumnoData.java
@@ -32,7 +32,7 @@ public class AlumnoData {
       
     }
     
-    public void guardarAlumno(Alumno alumno){
+public void guardarAlumno(Alumno alumno){
     
             String query = "INSERT INTO alumno (dni, apellido,nombre, fechaNacimiento, estado) "
                     + "VALUES (?, ?,?,?,?)";
@@ -60,35 +60,93 @@ public class AlumnoData {
              JOptionPane.showMessageDialog(null, "error al acceder a la tabla alumno");
     }}
     
-    public Alumno buscarAlumnoPor(int id) {
-       
-        String sql = "SELECT * FROM alumno WHERE idAlumno = ? AND estado = 1";
-         Alumno alumno = null;
+    
+    public void modificarAlumno(Alumno alumno) {
+        String query = "UPDATE alumno SET dni=?, apellido=?, nombre=?, fechaNacimiento=?"
+                + " WHERE idAlumno=?";
+        
         try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                
-                alumno = new Alumno();
-                alumno.setIdAlumno(id);
-                alumno.setDni(rs.getInt("dni"));
-                alumno.setApellido(rs.getString("apellido"));
-                alumno.setNombre(rs.getString("nombre"));
-                alumno.setFechaNac(rs.getDate("fechaNacimiento").toLocalDate());
-                alumno.setActivo(true);
-            }else{
-                JOptionPane.showMessageDialog(null, "No existe un alumno con ese ID");
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, alumno.getDni());
+            preparedStatement.setString(2, alumno.getApellido());
+            preparedStatement.setString(3, alumno.getNombre());
+            preparedStatement.setDate(4, Date.valueOf(alumno.getFechaNac()));       
+            preparedStatement.setInt(5, alumno.getIdAlumno()); 
+            
+            int filasModificadas = preparedStatement.executeUpdate();
+            
+            if (filasModificadas > 0) {
+                JOptionPane.showMessageDialog(null, "Se modificaron datos del alumno");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontro el alumno con el ID que ingresaste");
             }
-            ps.close();
-
+            
+            preparedStatement.close();
         } catch (SQLException e) {
-            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno");
-        }  
+         
+            JOptionPane.showMessageDialog(null, "Error para modificar datos del alumno"+ e.getMessage());
+        }
+    }
+
+     public void borradoLogicoAlumno(int idAlumno) {
+        String query = "UPDATE alumno SET estado = false WHERE idAlumno = ?";
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, idAlumno); // Asumiendo que idAlumno es el ID del alumno que deseas borrar
+            
+            int filasModificadas = preparedStatement.executeUpdate();
+            
+            if (filasModificadas > 0) {
+                JOptionPane.showMessageDialog(null, "El alumno fue borrado");
+            } else {
+                JOptionPane.showMessageDialog(null, "No se encontro el alumno con el ID proporcionado");
+            }
+            
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "Error al borrar el alumno");
+        }
+    }
+     
+     
+       
+     public Alumno buscarAlumnoPorId(int idAlumno) {
+        Alumno alumno = null;
+        String query = "SELECT * FROM alumno WHERE idAlumno = ?";
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, idAlumno);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                int dni = resultSet.getInt("dni");
+                String apellido = resultSet.getString("apellido");
+                String nombre = resultSet.getString("nombre");
+                java.sql.Date fechaNacimiento = resultSet.getDate("fechaNacimiento");
+                boolean estado = resultSet.getBoolean("estado");
+
+          
+                alumno = new Alumno(dni, apellido, nombre, fechaNacimiento.toLocalDate(), estado);
+                alumno.setIdAlumno(idAlumno);
+            }
+            
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+           
+        }
+        
         return alumno;
     }
-    
-    public Alumno buscarAlumnoPorDni(int dni) {
+     
+     
+     
+        
+   public Alumno buscarAlumnoPorDni(int dni) {
         Alumno alumno = null;
         String query = "SELECT * FROM alumno WHERE dni = ?";
         
@@ -123,8 +181,10 @@ public class AlumnoData {
         
         return alumno;
     }
+        
+        
     
-    public List<Alumno> listaAlumnos() {
+      public List<Alumno> listaAlumnos() {
         List<Alumno> listaAlumnos = new ArrayList<>();
         String query = "SELECT * FROM alumno WHERE estado = 1";
         
@@ -133,7 +193,7 @@ public class AlumnoData {
             ResultSet resultSet = preparedStatement.executeQuery();
             
             while (resultSet.next()) {
-                int idAlumno = resultSet.getInt("idAlumno");
+                int idAlumno = resultSet.getInt("iDalumno");
                 int dni = resultSet.getInt("dni");
                 String apellido = resultSet.getString("apellido");
                 String nombre = resultSet.getString("nombre");
@@ -159,57 +219,4 @@ public class AlumnoData {
         
         return listaAlumnos;
     }
-
-    public void modificarAlumno(Alumno alumno) {
-        String query = "UPDATE alumno SET dni=?, apellido=?, nombre=?, fechaNacimiento=?"
-                + " WHERE idAlumno=?";
-        
-        try {
-            PreparedStatement preparedStatement = con.prepareStatement(query);
-            preparedStatement.setInt(1, alumno.getDni());
-            preparedStatement.setString(2, alumno.getApellido());
-            preparedStatement.setString(3, alumno.getNombre());
-            preparedStatement.setDate(4, Date.valueOf(alumno.getFechaNac()));       
-            preparedStatement.setInt(5, alumno.getIdAlumno()); 
-            
-            int filasModificadas = preparedStatement.executeUpdate();
-            
-            if (filasModificadas > 0) {
-                JOptionPane.showMessageDialog(null, "Se modificaron datos del alumno");
-            } else {
-                JOptionPane.showMessageDialog(null, "No se encontró el alumno con el ID que ingresaste");
-            }
-            
-            preparedStatement.close();
-        } catch (SQLException e) {
-         
-            JOptionPane.showMessageDialog(null, "Error para modificar datos del alumno"+ e.getMessage());
-        }
-    }
-
-    public void borradoLogicoAlumno(int idAlumno) {
-        String query = "UPDATE alumno SET estado = false WHERE idAlumno = ?";
-        
-        try {
-            PreparedStatement preparedStatement = con.prepareStatement(query);
-            preparedStatement.setInt(1, idAlumno); // Asumiendo que idAlumno es el ID del alumno que deseas borrar
-            
-            int filasModificadas = preparedStatement.executeUpdate();
-            
-            if (filasModificadas > 0) {
-                JOptionPane.showMessageDialog(null, "El alumno fue borrado");
-            } else {
-                JOptionPane.showMessageDialog(null, "No se encontró el alumno con el ID proporcionado");
-            }
-            
-            preparedStatement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Error al borrar el alumno");
-        }
-    }
- 
 }
-   
-   
-
---
+++ src/universidadg12/accesoadatos/accesoadatos/MateriaData.java
@@ -0,0 +1,151 @@
+
+package universidadg12.accesoadatos;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import universidadg12.entidades.Materia;
+
+
+public class MateriaData {
+    private Connection con=null;
+
+    public MateriaData() {
+        this.con= Conexion.getConexion();
+    }
+    
+    public void guardarMateria(Materia materia){
+        
+        String sql ="INSERT INTO `materia`(`nombre`, `año`, `estado`)"
+                + "VALUES(?,?,?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnioMateria());
+            ps.setBoolean(3, materia.isActivo());
+            ps.executeUpdate();
+            
+            ResultSet rs = ps.getGeneratedKeys();
+            if(rs.next()){
+                materia.setIdMateria(1);
+                JOptionPane.showMessageDialog(null, "Materia agregada");
+            }
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(MateriaData.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+    
+   public void actualizarMateria(Materia materia) {
+        String query = "UPDATE materia SET nombre = ?, año = ?, estado = ? WHERE idMateria = ?";
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setString(1, materia.getNombre());
+            preparedStatement.setInt(2, materia.getAnioMateria());
+            preparedStatement.setBoolean(3, materia.isActivo());
+            preparedStatement.setInt(4, materia.getIdMateria());
+            preparedStatement.executeUpdate();
+            
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            
+        }
+    }
+
+    
+ public void eliminarMateria(int idMateria) {
+        String query = "UPDATE materia SET estado = 0 WHERE idMateria = ?";
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, idMateria);
+            preparedStatement.executeUpdate();
+            JOptionPane.showMessageDialog(null, "materia eliminada logicamente");
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "No se pudo eliminar la materia", "Error", JOptionPane.ERROR_MESSAGE);
+            
+        }
+    }
+    
+    
+    
+    
+    
+      public Materia buscarMateriaPorId(int idMateria) {
+        Materia materia = null;
+        String query = "SELECT * FROM materia WHERE idMateria = ? AND estado = 1";
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            preparedStatement.setInt(1, idMateria);
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String nombre = resultSet.getString("nombre");                
+                int año = resultSet.getInt("año");
+                boolean estado = resultSet.getBoolean("estado");
+
+                materia = new Materia();
+                materia.setIdMateria(idMateria);
+                materia.setNombre(nombre);
+                materia.setAnioMateria(año);
+                materia.setActivo(estado);
+            }
+            
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return materia;
+    }
+
+    
+    public List<Materia> obtenerMateriasActivas() {
+        List<Materia> materiasActivas = new ArrayList<>();
+        String query = "SELECT * FROM materia WHERE estado = 1";
+        
+        try {
+            PreparedStatement preparedStatement = con.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            while (resultSet.next()) {
+                int idMateria = resultSet.getInt("idMateria");
+                String nombre = resultSet.getString("nombre");
+                int año = resultSet.getInt("año");
+                boolean activo = resultSet.getBoolean("estado");
+
+                Materia materia = new Materia();
+                materia.setIdMateria(idMateria);
+                materia.setNombre(nombre);
+                materia.setAnioMateria(año);
+                materia.setActivo(activo);
+                
+                materiasActivas.add(materia);
+            }            
+            resultSet.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+                        JOptionPane.showMessageDialog(null, "No se pudo mostrar la losta, fijate que onda", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        
+        return materiasActivas;
+    }
+
+    public Iterable<Materia> listarMaterias() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
